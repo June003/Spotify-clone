b960e64237cad07bcfc7146ba9912720b451faaf
June003 <83715738+June003@users.noreply.github.com>
2024-06-02 03:14:34 +0530

    a2

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ async function displayAlbums() {
                 </svg>
             </div>
 
-            <img src="/songs/${folder}/cover.jpg" alt="">
+            <img src="./songs/${folder}/cover.jpg" alt="">
             <h2>${response.title}</h2>
             <p>${response.description}</p>
         </div>`
